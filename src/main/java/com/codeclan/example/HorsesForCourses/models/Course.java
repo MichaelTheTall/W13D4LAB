4473f98b5bfa449343e0e75e79b91ca36266dd85
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-25 11:01:59 +0100

    Course Class added

--- src/main/java/com/codeclan/example/HorsesForCourses/models/Course.java
+++ src/main/java/com/codeclan/example/HorsesForCourses/models/Course.java
@@ -1,4 +1,79 @@
 package com.codeclan.example.HorsesForCourses.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "town")
+    private String town;
+
+    @Column(name = "rating")
+    private int rating;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "course")
+    private List<Booking> bookingList;
+
+    public Course() {
+    }
+
+    public Course(String name, String town, int rating) {
+        this.name = name;
+        this.town = town;
+        this.rating = rating;
+        this.bookingList = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public List<Booking> getBookingList() {
+        return bookingList;
+    }
+
+    public void setBookingList(List<Booking> bookingList) {
+        this.bookingList = bookingList;
+    }
 }

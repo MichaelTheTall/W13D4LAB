871ed8170d93cf9eef2857a59f3ee1d21ed87c70
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-25 12:17:38 +0100

    custom routes 1-4 complete

--- src/main/java/com/codeclan/example/HorsesForCourses/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/HorsesForCourses/controllers/CourseController.java
@@ -1,4 +1,31 @@
 package com.codeclan.example.HorsesForCourses.controllers;
 
+import com.codeclan.example.HorsesForCourses.models.Course;
+import com.codeclan.example.HorsesForCourses.repositories.CourseRepository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/courses")
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping(value = "/rating/{rating}")
+    public List<Course> getCoursesByRating(@PathVariable int rating) {
+        return courseRepository.getCourseByRating(rating);
+    }
+
+    @GetMapping(value = "/customer/{id}")
+    public List<Course> getCoursesByRating(@PathVariable Long id) {
+        return courseRepository.getCoursesForCustomer(id);
+    }
+
+
 }
--- src/main/java/com/codeclan/example/HorsesForCourses/controllers/CustomerController.java
+++ src/main/java/com/codeclan/example/HorsesForCourses/controllers/CustomerController.java
@@ -1,4 +1,24 @@
 package com.codeclan.example.HorsesForCourses.controllers;
 
+import com.codeclan.example.HorsesForCourses.models.Customer;
+import com.codeclan.example.HorsesForCourses.repositories.CustomerRepository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/customers")
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/course/{id}")
+    public List<Customer> getCustomerByCourse(@PathVariable Long id) {
+        return customerRepository.getCustomerByCourse(id);
+    }
 }
